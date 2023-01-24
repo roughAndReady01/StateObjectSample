2e39181a47de7faece22f902000de16423710a35
Haruki Hanagata <haruzo28@icloud.com>
2023-01-25 04:28:07 +0900

    Initial Commit

---
+++ StateObjectSample/StateObjectSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectSampleApp.swift
+//  StateObjectSample
+//
+//  Created by 花形春輝 on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
